8321a21618e174d435b432f4e46a4954f73c914b
David-Angel-M <david.angelm28@gmail.com>
2022-11-30 16:21:02 +0000

    Add divison functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,10 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract" ) {
         displaySubtractQuestion(num1, num2);
-    } else {
+    } else if (gameType === "division" ) {
+        displayDivisionQuestion();
+    }
+    else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -89,7 +92,10 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else {
+    } else if (operator === "/") {
+        return [operand1/operand2, "division"];
+    }
+    else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -137,5 +143,20 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+
+}
+
+function displayDivisionQuestion() {
+    let num1 = 1;
+    let num2 = 2;
+    while(num1%num2 !== 0){
+        // Creates two random numbers between 1 and 25
+        num1 = Math.floor(Math.random() * 25) + 1;
+        num2 = Math.floor(Math.random() * 25) + 1;
+    }
+
+    document.getElementById('operand1').textContent = num1;
+    document.getElementById('operand2').textContent = num2;
+    document.getElementById('operator').textContent = "/";
 
 }
